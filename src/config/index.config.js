9e95cfbaa7ded218edb77a6864935c3e1edcca03
Pravar Anu <pravaranu6@gmail.com>
2023-04-27 22:09:31 +0530

    aws s3 configs and image upload functionality done

--- src/config/index.config.js
+++ src/config/index.config.js
@@ -6,7 +6,11 @@ const config = {
     PORT: process.env.PORT || 5000,
     MONGODB_URL: process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/ecomm",
     JWT_SECRET: process.env.JWT_SECRET || "supersecretkey",
-    JWT_EXPIRY: process.env.JWT_EXPIRY || "7d"
+    JWT_EXPIRY: process.env.JWT_EXPIRY || "7d",
+    S3_ACCESS_KEY: process.env.S3_ACCESS_KEY ||"youraccesskey",
+    S3_SECRET_ACCESS_KEY: process.env.S3_SECRET_ACCESS_KEY ||"yoursecretaccesskey",
+    S3_BUCKET_NAME: process.env.S3_BUCKET_NAME ||"youruniquebucketname",
+    S3_REGION: process.env.S3_REGION ||"yourregion"
 }
 
 export default config;
